9e8accefa31a260ed067fde7114a2a844d308ab5
Mathew B <148673495+mbrengman@users.noreply.github.com>
2025-11-06 14:33:55 -0700

    Update gui.py
    This file automatically loads the username from the current account.

--- gui.py
+++ gui.py
@@ -1,34 +1,96 @@
-from tkinter import Label, Tk, Frame, Button, Entry
-from tkinter import messagebox, Toplevel, OptionMenu, StringVar
-from helper import Warehouse, trace
+from tkinter import Label, Tk, Button, Entry, Radiobutton
+from tkinter import messagebox, Toplevel, OptionMenu
+from tkinter import IntVar, StringVar, Frame
+from helper import Warehouse, trace, get_date, date_selection
 from datetime import datetime
+from dataclasses import dataclass
+import getpass
 import logging
 
 log = logging.getLogger('__name__')
 logging.basicConfig(filename='info.log', level=logging.DEBUG)
 
-def item():
-    return Label(frame, text= frame.get())
+@dataclass
+class Flow:
+    income: str
+    debt: str
+
+def elements(base: Warehouse, flow: str):
+    number = base.length(flow)
+    index = number if number <= 10 else 10
+    return base.get_entries(flow)[:index+1]
 
 def end_process():
     answer = messagebox.askyesno('EXPENSE TRACKER', 'Close the program?')
     return answer    
 
 def project(user: str):
+    user = getpass.getuser()
+    base = Warehouse(user + '.db')
+    base.open()
+    display = Toplevel()
+    Label(display, text='Welcome to the expense tracker').grid(column=1, row=0)
+    Label(display, text='INCOME').grid(column=0, row=1)
+    Label(display, text='DEBT').grid(column=2, row=1)
+    fields = [elements(base, 'income'), elements(base, 'debt')]
+    [OptionMenu(display, variable=fields[i], values=fields[i]) for i in fields[0]]
+    Label(display, text=fields[0]).grid(column=0, row=len(fields[0])+2)
+    Label(display, text=fields[1]).grid(column=2, row=len(fields[1])+2)
+    Button(display, text='Delete entry').grid(column=2, row=8)
+    graphs: list[tuple[str]] = [
+        ('ALL', 0),
+        ('DAYS', 1),
+        ('WEEKS', 2),
+        ('MONTHS', 3)]
+    action = IntVar()
+    action.set(0)
+    [Radiobutton(display,
+        text=text,
+        command=lambda: \
+            show_graph(display, base, action.get())).grid(column=0, row=act+4)\
+            for text, act in graphs]
+    Label(display, text=action)
+
+def show_graph(display, base, action):
     try:
-        assert not user.isdigit()
-        assert not user[0].isdigit()
+        flow = Flow('income', 'debt')
+        _x_axis = None # Duration of measurement
+        income = base.get_entries(flow.income)
+        income_list = [item[2] for item in income]
+        del income
+        debt = base.get_entries(flow.debt)
+        debt_list = [item[2] for item in debt]
+        del debt
+        match action:
+            case 0:
+                total_income = base.get_total(flow.income)
+                total_debt = base.get_total(flow.debt)
+                return base.bar_graph(total_income, total_debt)
+            case 1:
+                return base.line_graph(income_list, debt_list)
+            case 2:
+                time = get_date()
+                current_date = date_selection(time, 30)
+                income_date = base.from_date(flow.income, current_date)
+                debt_date = base.from_date(flow.debt, current_date)
+                return base.line_graph(income_date, debt_date)
+            case 3:
+                pass
+    except Exception as error:
+         print(f' {datetime.now()} {error}\n\t{trace()}')
+
+def start():
+    try:
+        root = Tk()
+        Frame(root).grid(column=1, row=0)
+        Label(root, text="Welcome to the expense tracker").grid(column=1,row=1)
+        username = Entry(root, width=0)
+        user = Button(root, text='submit',
+            command=lambda: project(username.get()))
+        user.grid(column=0, row=2)
+        root.mainloop()
     except Exception as error:
         print(f' {datetime.now()} {error}\n\t{trace()}')
-    base = Warehouse(user)
-    base.open()
-    project = Toplevel()
-    Label(project, text='Welcome to the expense tracker').grid(column=1, row=0)
-    Label(project, text='INCOME').grid(column=0, row=1)
-    Label(project, text='DEBT').grid(column=2, row=1)
-    Label(project, text=base.present('income')).grid(column=0, row=2)
-    Label(project, text=base.present('debt')).grid(column=2, row=2)
-    Button(project, text='Delete entry')
 
 def delete(base, item: str, amount: float):
     types = ['income', 'debt']
@@ -43,33 +105,8 @@ def delete(base, item: str, amount: float):
     
 
 
-
-try:
-    exp = 'expenses.db'
-    # base = Warehouse('expenses.db')
-    # base.open()
-    root = Tk()
-    frame = Frame(root)
-    frame.grid()
-    top = Toplevel()
-    intro = Label(top, text='Welcome to the expense tracker').grid(column=0, row=0)
-    submit_button = Button(intro, text='submit', command=lambda: project(exp)).grid(column=0, row=3)
-    answer = Button(frame, text='Exit', command=end_process).grid(column=10, row=10)
-    if 1 == answer:
-        exit
-    Label(frame, text="Welcome to the expense tracker!!").grid(column=1, row=1)
-    Label(frame, text="Enter item").grid(column=3, row=3)
-    Entry(frame, width=50).grid(column=3, row=4)
-    Label(frame, text="Enter amount").grid(column=4, row=3)
-    Entry(frame, width=25).grid(column=4, row=4)
-    # clicked = None
-    # amount = None
-    # item, amount, clicked = (str(item), str(amount), str(clicked))
-    # OptionMenu(frame, clicked, 'PAYMENT TYPE', 'debt', 'income').grid(column=3, row=8)
-    Button(frame, text='Submit', command=item).grid(column=8, row=10)
-    root.mainloop()
-except Exception as error:
-    print(f' {datetime.now()} {error}\n\t{trace()}')
-
-# if __name__ == '__main__':
-#     root.mainloop()
+if __name__ == '__main__':
+    try:
+        start()
+    except Exception as error:
+        print(f' {datetime.now()} {error}\n\t{trace()}')
